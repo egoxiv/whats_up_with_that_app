0310b0464738efb5a3d8e31686986717b8ffda3b
Erik Gomez <egoxiv@gmail.com>
2016-04-21 11:21:20 -0700

    Adds defaults to model

--- seeds.js
+++ seeds.js
@@ -5,7 +5,7 @@ Question.remove({}, function(){
 
   Question.create([
     {title: "How do birds work?", body: "They can fly right?"},
-    {title: "How can I name a hurricane?", body: "I've got ideas."},
+    {title: "How can I name a hurricane?"},
     {title: "Why's coding gotta be so hard?", body: "Just do what I want."}
   ], function(err, questions){
     if (err) console.log(err);
