ceb8d5ba01d171b3e17a0bd10280a629c81b2002
Erik Gomez <egoxiv@gmail.com>
2016-04-21 11:06:57 -0700

    Adds ability to create question

--- controllers/questions_controller.js
+++ controllers/questions_controller.js
@@ -1,11 +1,24 @@
 var Question = require('../models/question');
-var questions = {}
+var questions = {};
 
 questions.index = function(req, res) {
   Question.find({}, function(err, questions) {
     if (err) return res.json(err);
     res.json(questions);
   });
-}
+};
+
+questions.create = function(req, res) {
+  console.log(req);
+  var question = new Question();
+  question.title = req.body.title;
+  question.body = req.body.body;
+  question.save(function(err) {
+    if(err) {
+      throw err;
+    }
+    res.json({message: 'added', success: true});
+  });
+};
 
 module.exports = questions;
